9a5e51edc412c244693765241fb9f4656ff4f87f
102errors <102errors@sina.com>
2017-10-10 19:27:26 +0800

    remove DDLog

--- MQTTClient-Codoon/Classes/MQTTLog.h
+++ MQTTClient-Codoon/Classes/MQTTLog.h
@@ -11,36 +11,31 @@
 #define MQTTLog_h
 
 
-// #ifdef LUMBERJACK
-//     #define LOG_LEVEL_DEF // DDLogLevel
-//     #import <CocoaLumberjack/CocoaLumberjack.h>
-//     #ifndef myLogLevel
-//         #ifdef DEBUG
-//             static const // DDLogLevel // DDLogLevel = // DDLogLevelWarning;
-//         #else
-//             static const // DDLogLevel // DDLogLevel = // DDLogLevelWarning;
-//         #endif
-//     #else
-//         static const // DDLogLevel // DDLogLevel = myLogLevel;
-//     #endif
-// #else
-//     #ifdef DEBUG
-//         #define // DDLogVerbose NSLog
-//         #define // DDLogWarn NSLog
-//         #define // DDLogInfo NSLog
-//         #define // DDLogError NSLog
-//     #else
-//         #define // DDLogVerbose(...)
-//         #define // DDLogWarn(...)
-//         #define // DDLogInfo(...)
-//         #define // DDLogError(...)
-//         #endif
-// #endif
-
-#define // DDLogVerbose(...)
-#define // DDLogWarn(...)
-#define // DDLogInfo(...)
-#define // DDLogError(...)
+#ifdef LUMBERJACK
+    #define LOG_LEVEL_DEF ddLogLevel
+    #import <CocoaLumberjack/CocoaLumberjack.h>
+    #ifndef myLogLevel
+        #ifdef DEBUG
+            static const DDLogLevel ddLogLevel = DDLogLevelWarning;
+        #else
+            static const DDLogLevel ddLogLevel = DDLogLevelWarning;
+        #endif
+    #else
+        static const DDLogLevel ddLogLevel = myLogLevel;
+    #endif
+#else
+    #ifdef DEBUG
+        #define DDLogVerbose NSLog
+        #define DDLogWarn NSLog
+        #define DDLogInfo NSLog
+        #define DDLogError NSLog
+    #else
+        #define DDLogVerbose(...)
+        #define DDLogWarn(...)
+        #define DDLogInfo(...)
+        #define DDLogError(...)
+        #endif
+#endif
 
 
 #endif /* MQTTLog_h */
